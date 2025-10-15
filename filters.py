abe2527fb086858b8918bdc807630efbe987d5f4
Erick Carrasco <erickype@hotmail.com>
2025-10-15 15:55:48 -0500

    Feature: Add function filter entries with more than five words and ordered by comments

--- filters.py
+++ filters.py
@@ -7,5 +7,16 @@ def count_words(title: str) -> int:
     words = re.sub(r"[^A-Za-z0-9\s]", "", title)
     return len(words.split())
 
+
+def filter_more_than_five(hacker_news_entries: list[HackerNewsEntry]) -> list[HackerNewsEntry]:
+    return sorted(
+        [e for e in hacker_news_entries if count_words(e.title) > 5],
+        key=lambda e: e.comments,
+        reverse=True,
+    )
+
+
+
 if __name__ == '__main__':
     entries = fetch_entries()
+    print(filter_more_than_five(entries))
