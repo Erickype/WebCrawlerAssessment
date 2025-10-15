775ae383140bc8ee51c9825595019cc8a44fdd5f
Erick Carrasco <erickype@hotmail.com>
2025-10-15 14:58:58 -0500

    Feature: Add data fetch from crawler

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 
+from crawler import fetch_entries
+
+def get_data():
+    return fetch_entries()
+
 st.set_page_config(page_title="WebCrawler", layout="wide")
 st.title("Web Crawler Assessment")
 
-entries = {}
+entries = get_data()
 st.success(f"Fetched {len(entries)} entries from Hacker News!")
 
 option = st.radio(
