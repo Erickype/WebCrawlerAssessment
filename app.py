a525d5bccd359f815d1e0859d0d7a8b644e23385
Erick Carrasco <erickype@hotmail.com>
2025-10-15 15:20:35 -0500

    Feature: Add datatable configurations and rendering

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 from crawler import fetch_entries
 
+@st.cache_data(ttl=600)
 def get_data():
     return fetch_entries()
 
@@ -17,4 +19,30 @@ option = st.radio(
         "More than five words (sorted by comments)",
         "Five or fewer words (sorted by points)",
     ],
+)
+
+#Data table
+df = pd.DataFrame(entries)
+
+st.data_editor(
+    df,
+    column_config={
+        "rank": st.column_config.NumberColumn(
+            "N°",
+            help="Hacker News Number ",
+        ),
+        "title": st.column_config.TextColumn(
+            "Title",
+            help="Title of the article",
+        ),
+        "points": st.column_config.NumberColumn(
+            "Points",
+            help="Points given to the article ",
+        ),
+        "comments": st.column_config.NumberColumn(
+            "N° comments",
+            help="Number of comments for the article",
+        ),
+    },
+    hide_index=True
 )
